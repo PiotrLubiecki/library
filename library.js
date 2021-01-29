2310bfba87c3098e53a531e902e5d3fa68228ce9
piotr13 <piotrek.lubiecki13@gmail.com>
2021-01-29 16:01:44 +0100

    updated with class instead of constructor

--- library.js
+++ library.js
@@ -1,7 +1,7 @@
 let myLibrary = [];
 
 const addBookForm = document.querySelector('#add-book-form')
-
+//hides or shows the submit form
 const newBookButton = document.querySelector('#new-book-button');
     newBookButton.addEventListener('click', () => {
         if(addBookForm.style.visibility === 'visible'){
@@ -9,27 +9,30 @@ const newBookButton = document.querySelector('#new-book-button');
         } else{
             addBookForm.style.visibility = 'visible';
         }
-    });     //hides or shows the submit form
-
+    });   
+//hides submit form
 const hideButton = document.querySelector('#hide-button');
     hideButton.addEventListener('click', () => {
         addBookForm.style.visibility = 'hidden';
-    });     //hides submit form
-
+    });     
+//adds book to the array and updates display
 const submitButton = document.querySelector('#submit-button')
     submitButton.addEventListener('click', () =>{
         clearDisplay();
         addBookToLibrary();
         displayLibrary(myLibrary);
         addBookForm.style.visibility = 'hidden';
-    })  //adds book to the array and updates display
+    })  
 
-function Book(id, title, author, pages, isRead) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
+//class to make new book objects
+class Book {
+    constructor(id, title, author, pages, isRead){
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+        }
 }
 
 function addBookToLibrary() {
@@ -73,6 +76,7 @@ function displayLibrary(array) {
             sp2.classList.add('small-p');
             bp3.classList.add('big-p');
             sp3.classList.add('small-p');
+            //sets button text and color according to read status 
             if(array[i].isRead == 'true'){
                 bookReadButton.classList.add('book-read');
                 bookReadButton.textContent = 'Read';
@@ -89,7 +93,7 @@ function displayLibrary(array) {
             bp3.textContent = array[i].pages;
             sp3.textContent = 'Pages';
             deleteBook.innerHTML = '<icon class="material-icons">delete</icon>';
-            
+            //changes isRead value and updates display
             bookReadButton.addEventListener('click', () =>{
                 if(bookReadButton.textContent === 'Read'){
                     array[i].isRead = 'false';
@@ -99,8 +103,8 @@ function displayLibrary(array) {
                 clearDisplay();
                 saveLocalStorage();
                 displayLibrary(myLibrary);
-            }); //changes isRead value and updates display
-
+            }); 
+            //removes book from array and updates display
             deleteBook.addEventListener('click', () => {
                 myLibrary.splice(deleteBook.parentElement.id, 1);
                 deleteBook.parentElement.remove();
@@ -108,7 +112,7 @@ function displayLibrary(array) {
                 clearDisplay();
                 saveLocalStorage();
                 displayLibrary(myLibrary);
-            }); //removes book from array and updates display
+            }); 
 
             book.appendChild(bp1);
             book.appendChild(sp1);
@@ -129,14 +133,14 @@ function clearDisplay() {
             bookshelf.removeChild(bookshelf.firstChild);
         }
 }   
-
+//updates IDs of books when book is removed
 function updateID(array) {
     if(array.length != 0){
         for(let i = 0; i < array.length; i++){
             array[i].id = i;
         }
     }
-}   //updates IDs of books when book is removed
+}   
 
 function saveLocalStorage() {
     localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
